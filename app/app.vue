723e5958efccf0a605c53295a226b9542813cc2b
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-09-13 20:54:51 +0700

    Add tailwindcss & prettier ✨

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <div class="container mx-auto flex h-screen items-center justify-center">
+        <h1 class="text-8xl font-bold underline">Hello NUXT!</h1>
+    </div>
 </template>
